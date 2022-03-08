3c1a6669c22593869e77f28f67184e40a6274b3f
llunerti <leonardo.lunerti@cern.ch>
2022-03-08 17:57:29 +0100

    print timing info

--- python/hnl_tree_analyzer.py
+++ python/hnl_tree_analyzer.py
@@ -14,6 +14,10 @@ dataset_to_process = sys.argv[2]
 with open(configFileName, "r") as f:
     config = json.loads(f.read())
 
+histoConfigFileName = "/afs/cern.ch/work/l/llunerti/private/hnlTreeAnalyzer/cfg/histos.json"
+with open(histoConfigFileName, "r") as f:
+    histos = json.loads(f.read())
+
 #get input files
 inputFileName_list = config[dataset_to_process]["slimmed_file_name_list"]
 dataset_category = config[dataset_to_process]["dataset_category"]
@@ -30,6 +34,7 @@ print("event_weight: {}".format(event_weight))
 
 chain = ROOT.TChain('slimmed_tree')
 for inputFileName in inputFileName_list:
+    print("Adding {} to the chain...".format(inputFileName))
     chain.Add(inputFileName)
 
 cat_dict = {"chargedHnl":"C_mu1_charge_ptBest+C_pi_charge_ptBest != 0",
@@ -38,6 +43,8 @@ cat_dict = {"chargedHnl":"C_mu1_charge_ptBest+C_pi_charge_ptBest != 0",
 n_cuts = 20
 sel_cuts = {"C_Hnl_lxy_ptBest":numpy.linspace(-1,30,n_cuts),
             "C_Hnl_pt_ptBest": numpy.linspace(-1,30,n_cuts)}
+
+n_threads = 8
 
 for cat in cat_dict:
     input_file_name = inputFileName_list[0].split("/")[-1].split(".")[0]
@@ -49,41 +56,12 @@ for cat in cat_dict:
     subprocess.call(['mkdir','-p',outputDirName])
     outputFile = ROOT.TFile(os.path.join(outputDirName,outputFileName),"RECREATE")
     
-    m_hnl_pt           = ("h_hnl_pt"          ,";HNL p_{T} [GeV];Events", 34, 6., 40.)
-    m_hnl_preFit_mass  = ("h_hnl_preFit_mass" ,";HNL (pre-fit) mass [GeV];Events", 40, 0.25, 6.)
-    m_hnl_postFit_mass = ("h_hnl_postFit_mass",";HNL (post-fit) mass [GeV];Events", 40, 0.25, 6.)
-    m_hnl_lxy          = ("h_hnl_lxy"         ,";HNL L_{xy} [cm];Events", 20, 0., 40.)
-    m_hnl_vtx_prob     = ("h_hnl_vtx_prob"    ,";SV probability;Events", 50, 0., 1.)
-    m_hnl_vtx_cos2D    = ("h_hnl_vtx_cos2D"   ,";SV cos2D;Events", 100, -1., 1.)
-    m_hnl_vtx_dispSig  = ("h_hnl_vtx_dispSig" ,";SV displacement significance;Events", 100, 0., 100.)
-    
-    m_mu1_pt           = ("h_mu1_pt"    ,";#mu_{1} p_{T} [GeV];Events", 24, 6., 30.)
-    m_mu1_eta          = ("h_mu1_eta"   ,";#mu_{1} #eta;Events", 20, -2.5, 2.5)
-    m_mu1_ip_z         = ("h_mu1_ip_z"  ,";#mu_{1} z IP [cm];Events", 20, 0., 5.)
-    m_mu1_ip_xy        = ("h_mu1_ip_xy" ,";#mu_{1} xy IP [cm];Events", 20, 0., 5.)
-    m_mu1_ips_z        = ("h_mu1_ips_z" ,";#mu_{1} z IPS;Events", 20, 0., 200.)
-    m_mu1_ips_xy       = ("h_mu1_ips_xy",";#mu_{1} xy IPS;Events", 20, 0., 200.)
-    
-    m_mu2_pt           = ("h_mu2_pt"  ,";#mu_{2} p_{T} [GeV];Events", 24, 6., 30.)
-    m_mu2_eta          = ("h_mu2_eta" ,";#mu_{2} #eta;Events", 20, -2.5, 2.5)
-    
-    m_pi_pt            = ("h_pi_pt"    ,";#pi p_{T} [GeV];Events", 40, 0., 20.)
-    m_pi_eta           = ("h_pi_eta"   ,";#pi #eta;Events", 20, -2.5, 2.5)
-    m_pi_ip_z          = ("h_pi_ip_z"  ,";#pi z IP [cm];Events", 20, 0., 5.)
-    m_pi_ip_xy         = ("h_pi_ip_xy" ,";#pi xy IP [cm];Events", 20, 0., 5.)
-    m_pi_ips_z         = ("h_pi_ips_z" ,";#pi z IPS;Events", 20, 0., 200.)
-    m_pi_ips_xy        = ("h_pi_ips_xy",";#pi xy IPS;Events", 20, 0., 200.)
-    
-    m_pv               = ("h_pv",";Number of primary vertices;Events", 60, 0., 60.)
-    m_pu_trueInt       = ("h_pu_trueInt",";Number of true PU interactions;Events", 60, 0., 60.)
-    
-    ROOT.EnableImplicitMT(8)
-    
+    ROOT.EnableImplicitMT(n_threads)
     df = ROOT.RDataFrame(chain)
     
     df = df.Define("weight",str(event_weight))    
     df = df.Filter(cat_dict[cat] ,cat+" category")
-    df = df.Filter("mu9_ip6_matched_ptBest>0 || mu12_ip6_matched_ptBest>0" ,"mu9_ip6 or mu12_ip6 matched")
+    df = df.Filter("mu9_ip6_matched_ptBest>0" ,"mu9_ip6 matched")
     tot_events = df.Count().GetValue()
 
     #build signal selection efficiency csv file 
@@ -110,58 +88,23 @@ for cat in cat_dict:
     #get mc truth in case of signal sample
     if dataset_category == "signal":
         df = df.Filter("C_mu1_isHnlDaughter_ptBest>0 && C_pi_isHnlDaughter_ptBest>0 && C_mu2_isHnlBrother_ptBest>0")
-    
-    h_hnl_pt           = df.Histo1D(m_hnl_pt          ,"C_Hnl_pt_ptBest","weight")
-    h_hnl_preFit_mass  = df.Histo1D(m_hnl_preFit_mass ,"C_Hnl_preFit_mass_ptBest","weight")
-    h_hnl_postFit_mass = df.Histo1D(m_hnl_postFit_mass,"C_Hnl_mass_ptBest","weight")
-    h_hnl_lxy          = df.Histo1D(m_hnl_lxy         ,"C_Hnl_lxy_ptBest","weight")
-    h_hnl_vtx_prob     = df.Histo1D(m_hnl_vtx_prob    ,"C_Hnl_vertex_prob_ptBest","weight")
-    h_hnl_vtx_cos2D    = df.Histo1D(m_hnl_vtx_cos2D   ,"C_Hnl_vertex_cos2D_ptBest","weight")
-    h_hnl_vtx_dispSig  = df.Histo1D(m_hnl_vtx_dispSig ,"C_Hnl_vertex_sig_ptBest","weight")
-                                                      
-    h_mu2_pt           = df.Histo1D(m_mu2_pt          ,"C_mu2_pt_ptBest","weight")
-    h_mu2_eta          = df.Histo1D(m_mu2_eta         ,"C_mu2_eta_ptBest","weight")
-                                          
-    h_mu1_pt           = df.Histo1D(m_mu1_pt          ,"C_mu1_pt_ptBest","weight")
-    h_mu1_eta          = df.Histo1D(m_mu1_eta         ,"C_mu1_eta_ptBest","weight")
-    h_mu1_ip_z         = df.Histo1D(m_mu1_ip_z        ,"C_mu1_ip_z_ptBest","weight")
-    h_mu1_ip_xy        = df.Histo1D(m_mu1_ip_xy       ,"C_mu1_ip_xy_ptBest","weight")
-    h_mu1_ips_z        = df.Histo1D(m_mu1_ips_z       ,"C_mu1_ips_z_ptBest","weight")
-    h_mu1_ips_xy       = df.Histo1D(m_mu1_ips_xy      ,"C_mu1_ips_xy_ptBest","weight")
-                                                      
-    h_pi_pt            = df.Histo1D(m_pi_pt           ,"C_pi_pt_ptBest","weight")
-    h_pi_eta           = df.Histo1D(m_pi_eta          ,"C_pi_eta_ptBest","weight")
-    h_pi_ip_z          = df.Histo1D(m_pi_ip_z         ,"C_pi_ip_z_ptBest","weight")
-    h_pi_ip_xy         = df.Histo1D(m_pi_ip_xy        ,"C_pi_ip_xy_ptBest","weight")
-    h_pi_ips_z         = df.Histo1D(m_pi_ips_z        ,"C_pi_ips_z_ptBest","weight")
-    h_pi_ips_xy        = df.Histo1D(m_pi_ips_xy       ,"C_pi_ips_xy_ptBest","weight")
-    
-    h_pv               = df.Histo1D(m_pv              ,"nPV","weight")
-    h_pu_trueInt       = df.Histo1D(m_pu_trueInt      ,"nPU_trueInt","weight")
-    
-    h_hnl_pt          .Write()
-    h_hnl_preFit_mass .Write()
-    h_hnl_postFit_mass.Write()
-    h_hnl_lxy         .Write()
-    h_hnl_vtx_prob    .Write()
-    h_hnl_vtx_cos2D   .Write()
-    h_hnl_vtx_dispSig .Write()
-    h_mu2_pt          .Write()
-    h_mu2_eta         .Write()
-    h_mu1_pt          .Write()
-    h_mu1_eta         .Write()
-    h_mu1_ip_z        .Write()
-    h_mu1_ip_xy       .Write()
-    h_mu1_ips_z       .Write()
-    h_mu1_ips_xy      .Write()
-    h_pi_pt           .Write()
-    h_pi_eta          .Write()
-    h_pi_ip_z         .Write()
-    h_pi_ip_xy        .Write()
-    h_pi_ips_z        .Write()
-    h_pi_ips_xy       .Write()
-    h_pv              .Write()
-    h_pu_trueInt      .Write()
+
+    #book histograms
+    histo_dict = {}
+    for histo_name in histos:
+        title = str(histos[histo_name]["title"])
+        nbins = int(histos[histo_name]["nbins"])
+        xlow  = float(histos[histo_name]["xlow"])
+        xhigh = float(histos[histo_name]["xhigh"])
+        histo_model = (histo_name,title,nbins,xlow,xhigh)
+
+        var_name = str(histos[histo_name]["var"])
+
+        histo_dict[histo_name]= df.Histo1D(histo_model,var_name,"weight")
+
+    #save histograms
+    for histo_name in histo_dict:
+        histo_dict[histo_name].Write()
     
     outputFile.Close()
     
@@ -169,4 +112,5 @@ for cat in cat_dict:
     print("-- REPORT --")
     r.Print()
 
-#print("--- {} seconds ---".format(time.time() - start_time))
+print("Output saved in {}".format(os.path.join(outputDirName,outputFileName)))
+print("---{} threads, {} seconds ---".format(n_threads,time.time() - start_time))
