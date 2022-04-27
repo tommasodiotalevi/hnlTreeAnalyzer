7b8d56c5dec385f2f5c578c26f0dd9e60d709e21
llunerti <leonardo.lunerti@cern.ch>
2022-04-27 17:17:09 +0200

    functions to be used in tree_analyzer.py

---
+++ interface/df_tools.h
@@ -0,0 +1,47 @@
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TLatex.h"
+#include "Math/Vector4D.h"
+#include "TStyle.h"
+
+using namespace ROOT;
+using namespace ROOT::VecOps;
+
+size_t get_best_cand_idx(RVec<double> cand_var)
+{
+  float var_best = -1;
+  size_t idx_best = 9999;
+  for (unsigned i=0; i<cand_var.size(); ++i)
+  {
+    if(cand_var.at(i)>var_best)
+    {
+      var_best = cand_var.at(i);
+      idx_best = i;
+    }
+  }
+
+  return idx_best;
+}
+
+RVec<short> get_mu_trigger_matching(RVec<short> trigger_match, RVec<unsigned> trig_mu_idx, RVec<unsigned> mu_idx)
+{
+  RVec<short> mu_has_matched_trigger(mu_idx.size());
+  //mu_has_matched_trigger.reserve(mu_idx.size());
+  //for(unsigned i=0; i<mu_has_matched_trigger.size(); ++i){
+  //  mu_has_matched_trigger[i] = 0;
+  //}
+
+  for(unsigned i_trigMatch=0; i_trigMatch<trigger_match.size(); ++i_trigMatch){
+
+    if (!(trigger_match.at(i_trigMatch)>0)) continue;
+
+    for(unsigned i_mu=0; i_mu<mu_idx.size(); ++i_mu){
+      if (mu_idx[i_mu] == trig_mu_idx[i_trigMatch])
+        mu_has_matched_trigger[i_mu] = 1;
+    }
+  }
+
+  return mu_has_matched_trigger;
+}
