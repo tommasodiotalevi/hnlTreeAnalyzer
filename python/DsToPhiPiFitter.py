3417b6c1b39f03b7c99d753e8f5a4f901374aeb6
llunerti <leonardo.lunerti@cern.ch>
2022-04-27 17:17:33 +0200

    minor

--- python/DsToPhiPiFitter.py
+++ python/DsToPhiPiFitter.py
@@ -1,5 +1,11 @@
 import argparse
 import sys
+import os
+import subprocess
+
+output_dir = "out_fitter/ds"
+subprocess.call("mkdir -p "+output_dir,shell=True)
+
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("filename",help="Path to the input rootfile")
 parser.add_argument("histoName",help="Name of the input histogram")
@@ -9,7 +15,7 @@ import ROOT
 
 #Import TH1F into a RooDataHist	
 inputFile  = ROOT.TFile(args.filename)
-inputHisto = ROOT.TH1F(inputFile.Get(args.histoName))
+inputHisto = ROOT.TH1D(inputFile.Get(args.histoName))
 histoTitle = inputHisto.GetTitle()
 w = ROOT.RooWorkspace("w","workspace")
 
@@ -19,21 +25,21 @@ invMass = ROOT.RooRealVar("invMass","#mu#mu#pi invariant mass",1.8,2.1)
 inputDataHist = ROOT.RooDataHist("inputDataHist","inputDataHist",ROOT.RooArgList(invMass),inputHisto)
 
 #Defining variables
-mean_1    = ROOT.RooRealVar("mean_1","mean_1",1.96,1.98) 
-sigma_1   = ROOT.RooRealVar("sigma_1","sigma_1",0.001,0.05)
-mean_2    = ROOT.RooRealVar("mean_2","mean_2",1.96,1.98)
-sigma_2   = ROOT.RooRealVar("sigma_2","sigma_2",0.001,0.05)
+mean_D    = ROOT.RooRealVar("mean_D","mean_D",1.8695)    
+sigma_D   = ROOT.RooRealVar("sigma_D","sigma_D",0.01,0.1)
+mean_Ds   = ROOT.RooRealVar("mean_Ds","mean_Ds",1.96835)   
+sigma_Ds  = ROOT.RooRealVar("sigma_Ds","sigma_Ds",0.01,0.1)
 c_exp     = ROOT.RooRealVar("c_exp","c_exp",-1.5,-1.1)
-coeff_1   = ROOT.RooRealVar("coeff_1","coeff_1",0.,1.)
-n_sig     = ROOT.RooRealVar("n_sig","n_sig",10,100000)
+coeff     = ROOT.RooRealVar("coeff","coeff",0.,1.)
+n_D       = ROOT.RooRealVar("n_D","n_D",10,100000)
+n_Ds      = ROOT.RooRealVar("n_Ds","n_Ds",10,100000)
 n_bkg     = ROOT.RooRealVar("n_bkg","n_bkg",10,100000)
 
 #Defining signal and background pdfs
-gaussian_1 = ROOT.RooGaussian("gaussian_1","gaussian_1",invMass, mean_1, sigma_1)
-gaussian_2 = ROOT.RooGaussian("gaussian_2","gaussian_2",invMass, mean_2, sigma_2)
+gaussian_D = ROOT.RooGaussian("gaussian_D","gaussian_D",invMass, mean_D, sigma_D)
+gaussian_Ds = ROOT.RooGaussian("gaussian_Ds","gaussian_Ds",invMass, mean_Ds, sigma_Ds)
 expo       = ROOT.RooExponential("expo","expo",invMass,c_exp)
-sig        = ROOT.RooAddPdf("sig","sig",ROOT.RooArgList(gaussian_1,gaussian_2),ROOT.RooArgList(coeff_1),ROOT.kTRUE)
-model      = ROOT.RooAddPdf("model","D_{s}#rightarrow #phi#pi decay model", ROOT.RooArgList(sig, expo), ROOT.RooArgList(n_sig,n_bkg))
+model      = ROOT.RooAddPdf("model","D_{(s)}#rightarrow #phi#pi decay model", ROOT.RooArgList(gaussian_D,gaussian_Ds, expo), ROOT.RooArgList(n_D,n_Ds,n_bkg))
 
 frame = invMass.frame()
 
@@ -49,20 +55,23 @@ getattr(w,'import')(model)
 chiSquare = frame.chiSquare()
 t1 = ROOT.TPaveText(0.67,0.7,0.9,0.88,"brNDC")
 t1.AddText("#chi^{{2}}/dof = {}".format(round(chiSquare,2)))
-t1.AddText("N_{{sig}} = {} #pm {}".format(round(n_sig.getValV(),0),round(n_sig.getError(),0)))
-#t1.AddText("M_{{D_{{s}}}} = {} #pm {} MeV".format(round(mean_2.getValV()*1e3,1),round(mean_2.getError()*1e3,1)))
-#t1.AddText("M_{{D}} = {} #pm {} MeV".format(round(mean_1.getValV()*1e3,1),round(mean_1.getError()*1e3,1)))
+t1.AddText("N_{{D_{{s}}}} = {} #pm {}".format(round(n_Ds.getValV(),0),round(n_Ds.getError(),0)))
+t1.AddText("N_{{D}} = {} #pm {}".format(round(n_D.getValV(),0),round(n_D.getError(),0)))
+t1.AddText("N_{{bkg}} = {} #pm {}".format(round(n_bkg.getValV(),0),round(n_bkg.getError(),0)))
+t1.AddText("M_{{D_{{s}}}} = {} #pm {} MeV".format(round(mean_Ds.getValV()*1e3,1),round(mean_Ds.getError()*1e3,1)))
+t1.AddText("M_{{D}} = {} #pm {} MeV".format(round(mean_D.getValV()*1e3,1),round(mean_D.getError()*1e3,1)))
 t1.SetTextColor(ROOT.kBlack)
 
 #Draw fitted functions
 print("chi-square/ndof = {} \n".format(frame.chiSquare()))
-model.plotOn(frame,ROOT.RooFit.Name("gaussian_1"),ROOT.RooFit.Components("gaussian_1"),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
-model.plotOn(frame,ROOT.RooFit.Name("gaussian_2"),ROOT.RooFit.Components("gaussian_2"),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
+model.plotOn(frame,ROOT.RooFit.Name("gaussian_D"),ROOT.RooFit.Components("gaussian_D"),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
+model.plotOn(frame,ROOT.RooFit.Name("gaussian_Ds"),ROOT.RooFit.Components("gaussian_Ds"),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kViolet))
 model.plotOn(frame,ROOT.RooFit.Name("background"),ROOT.RooFit.Components("expo")      ,ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kGreen))
 
 #Build legend
 leg = ROOT.TLegend(0.12,0.75,0.25,0.88)
-leg.AddEntry(frame.findObject("gaussian_2"),"D_{s}","l")
+leg.AddEntry(frame.findObject("gaussian_Ds"),"D_{s}","l")
+leg.AddEntry(frame.findObject("gaussian_D"),"D","l")
 leg.AddEntry(frame.findObject("background"),"bkg","l")
 leg.SetBorderSize(0)
 
@@ -72,11 +81,11 @@ frame.GetXaxis().SetTitle("m_{#mu#mu#pi} [GeV]")
 frame.Draw()
 t1.Draw("same")
 leg.Draw("same")
-w.writeToFile("Out_fitter/model"+args.histoName+".root")
-c.SaveAs("Out_fitter/fitted_"+args.histoName+"_mass.png")
-c.SaveAs("Out_fitter/fitted_"+args.histoName+"_mass.root")
+w.writeToFile(os.path.join(output_dir,"model_"+args.histoName+".root"))
+c.SaveAs(os.path.join(output_dir,"fitted_"+args.histoName+".png"))
+c.SaveAs(os.path.join(output_dir,"fitted_"+args.histoName+".root"))
 inputFile.Close()
-outputFile = ROOT.TFile.Open("Out_fitter/fitted_"+args.histoName+"_parameters.root","RECREATE")
+outputFile = ROOT.TFile.Open(os.path.join(output_dir,"fitted_"+args.histoName+"_parameters.root"),"RECREATE")
 c.Write()
 result.Write()
 outputFile.Close()
