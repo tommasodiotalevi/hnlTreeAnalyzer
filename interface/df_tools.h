e6745c30bf3d4ac6685ab5ce88f0fb63719fc114
llunerti <leonardo.lunerti@cern.ch>
2022-10-27 12:05:02 +0200

    several new helper functions

--- interface/df_tools.h
+++ interface/df_tools.h
@@ -59,6 +59,176 @@ size_t get_best_cand_idx(RVec<double> cand_var, RVec<double> mu1mu2_var, RVec<un
   return idx_best;
 }
 
+//select the best candidate based on difference between two variables,
+RVec<short> and_3vec(RVec<short> var1, RVec<short> var2,RVec<short> var3)
+{
+
+  size_t n_cand = var1.size();
+  RVec<Double_t> and_result(n_cand,0.);
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<n_cand; ++i)
+  {
+    short res = var1[i]*var2[i]*var3[i];
+    and_result[i] = res;
+  }
+
+  return and_result;
+}
+
+//select the best candidate based on difference between two variables,
+size_t get_maxDiff_cand_idx(RVec<double> cand_var1, RVec<double> cand_var2)
+{
+  float diff_best = -99999.;
+  size_t idx_best = 9999;
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<cand_var1.size(); ++i)
+  {
+    float diff = std::abs(cand_var1.at(i)-cand_var2.at(i));
+    if(diff>diff_best)
+    {
+      diff_best = diff;
+      idx_best = i;
+    }
+  }
+
+  return idx_best;
+}
+
+size_t get_maxRatio_cand_idx(RVec<double> cand_var1, RVec<double> cand_var2)
+{
+  float ratio_best = -99999.;
+  size_t idx_best = 9999;
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<cand_var1.size(); ++i)
+  {
+    float ratio = cand_var1.at(i)/cand_var2.at(i);
+    if(ratio>ratio_best)
+    {
+      ratio_best = ratio;
+      idx_best = i;
+    }
+  }
+
+  return idx_best;
+}
+
+size_t get_maxWeightedRatio_wPosCond_cand_idx(RVec<double> cand_var1, RVec<double> cand_var2, RVec<double> cand_var3,RVec<double> weight)
+{
+  float ratio_best = -99999.;
+  size_t idx_best = 9999;
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<cand_var1.size(); ++i)
+  {
+    if(cand_var3.at(i)<0) continue;
+    float ratio = (cand_var1.at(i)/cand_var2.at(i))*weight.at(i);
+    if(ratio>ratio_best)
+    {
+      ratio_best = ratio;
+      idx_best = i;
+    }
+  }
+
+  return idx_best;
+}
+
+size_t get_maxRatio_wPosCond_cand_idx(RVec<double> cand_var1, RVec<double> cand_var2, RVec<double> cand_var3)
+{
+  float ratio_best = -99999.;
+  size_t idx_best = 9999;
+  //size_t idx_best = 0;
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<cand_var1.size(); ++i)
+  {
+    if(cand_var3.at(i)<0) continue;
+    float ratio = cand_var1.at(i)/cand_var2.at(i);
+    if(ratio>ratio_best)
+    {
+      ratio_best = ratio;
+      idx_best = i;
+    }
+  }
+
+  //if no candidates with positive condition has been found,
+  //the condition is released
+  if (idx_best==9999)
+  {
+    for (unsigned i=0; i<cand_var1.size(); ++i)
+    {
+      float ratio = cand_var1.at(i)/cand_var2.at(i);
+      if(ratio>ratio_best)
+      {
+        ratio_best = ratio;
+        idx_best = i;
+      }
+    }
+  }
+
+  return idx_best;
+}
+
+size_t get_cand_idx_custom(RVec<double> cand_var1, RVec<double> cand_var2,RVec<double> cand_var3,RVec<double> cond,RVec<double> cand_var4,RVec<double> cand_var5)
+{
+  float sum_best = -99999.;
+  size_t idx_best = 9999;
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<cand_var1.size(); ++i)
+  {
+    if (cond.at(i)<0) continue;
+    float sum = (cand_var1.at(i)+cand_var2.at(i)+cand_var3.at(i))*(cand_var4.at(i)/cand_var5.at(i));
+    if(sum>sum_best)
+    {
+      sum_best = sum;
+      idx_best = i;
+    }
+  }
+
+  return idx_best;
+}
+
+size_t get_maxSum_cand_idx(RVec<double> cand_var1, RVec<double> cand_var2,RVec<double> cand_var3)
+{
+  float sum_best = -99999.;
+  size_t idx_best = 9999;
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<cand_var1.size(); ++i)
+  {
+    float sum = cand_var1.at(i)+cand_var2.at(i)+cand_var3.at(i);
+    if(sum>sum_best)
+    {
+      sum_best = sum;
+      idx_best = i;
+    }
+  }
+
+  return idx_best;
+}
+
+size_t get_maxSum_cand_idx(RVec<double> cand_var1, RVec<double> cand_var2)
+{
+  float sum_best = -99999.;
+  size_t idx_best = 9999;
+
+  //select best candidate based on cand_var
+  for (unsigned i=0; i<cand_var1.size(); ++i)
+  {
+    float sum = std::abs(cand_var1.at(i)+cand_var2.at(i));
+    if(sum>sum_best)
+    {
+      sum_best = sum;
+      idx_best = i;
+    }
+  }
+
+  return idx_best;
+}
+
 RVec<short> get_mu_trigger_matching(RVec<short> trigger_match, RVec<unsigned> trig_mu_idx, RVec<unsigned> mu_idx)
 {
   RVec<short> mu_has_matched_trigger(mu_idx.size());
@@ -76,9 +246,44 @@ RVec<short> get_mu_trigger_matching(RVec<short> trigger_match, RVec<unsigned> tr
   return mu_has_matched_trigger;
 }
 
+RVec<short> get_mu_trigger_matching(RVec<short> trigger_match, RVec<float> trigger_dr, RVec<unsigned> trig_mu_idx, RVec<unsigned> mu_idx)
+{
+  RVec<short> mu_has_matched_trigger(mu_idx.size());
+
+  for(unsigned i_trigMatch=0; i_trigMatch<trigger_match.size(); ++i_trigMatch){
+
+    if (!(trigger_match.at(i_trigMatch)>0)) continue;
+    if (trigger_dr.at(i_trigMatch)>0.005) continue;
+
+    for(unsigned i_mu=0; i_mu<mu_idx.size(); ++i_mu){
+      if (mu_idx[i_mu] == trig_mu_idx[i_trigMatch])
+        mu_has_matched_trigger[i_mu] = 1;
+    }
+  }
+
+  return mu_has_matched_trigger;
+}
+
+RVec<float> get_mu_trigger_dr(RVec<short> trigger_match, RVec<float> trigger_dr, RVec<unsigned> trig_mu_idx, RVec<unsigned> mu_idx)
+{
+  RVec<float> muTrig_dr(mu_idx.size(),9999.);
+
+  for(unsigned i_trigMatch=0; i_trigMatch<trigger_match.size(); ++i_trigMatch){
+
+    if (!(trigger_match.at(i_trigMatch)>0)) continue;
+
+    for(unsigned i_mu=0; i_mu<mu_idx.size(); ++i_mu){
+      if (mu_idx[i_mu] == trig_mu_idx[i_trigMatch])
+        muTrig_dr[i_mu] = trigger_dr[i_trigMatch];
+    }
+  }
+
+  return muTrig_dr;
+}
+
 //this is to get candidate invariant mass with different 
 //final state particle mass ipothesis
-RVec<Double_t> get_PxPyPzE_newM_HnlInvariantMass(RVec<Double_t> px_1, RVec<Double_t> py_1, RVec<Double_t> pz_1, RVec<Double_t> E_1, Double_t m_1,
+RVec<Double_t> get_PxPyPzE_newM_2PartInvMass(RVec<Double_t> px_1, RVec<Double_t> py_1, RVec<Double_t> pz_1, RVec<Double_t> E_1, Double_t m_1,
 		                                 RVec<Double_t> px_2, RVec<Double_t> py_2, RVec<Double_t> pz_2, RVec<Double_t> E_2, Double_t m_2)
 {
   size_t n_cand = px_1.size();
@@ -95,6 +300,75 @@ RVec<Double_t> get_PxPyPzE_newM_HnlInvariantMass(RVec<Double_t> px_1, RVec<Doubl
     p4_newM_1.SetPtEtaPhiM(p4_1.Pt(),p4_1.Eta(),p4_1.Phi(),m_1);
     p4_newM_2.SetPtEtaPhiM(p4_2.Pt(),p4_2.Eta(),p4_2.Phi(),m_2);
     cand_inv_mass[i]=(p4_newM_1+p4_newM_2).M();
+  }
+
+  return cand_inv_mass;
+
+}
+
+RVec<Double_t> get_PtEtaPhi_newM_3PartInvMass(RVec<Double_t> pt_1, RVec<Double_t> eta_1, RVec<Double_t> phi_1, Double_t newM_1,
+		                              RVec<Double_t> pt_2, RVec<Double_t> eta_2, RVec<Double_t> phi_2, Double_t newM_2,
+		                              RVec<Double_t> pt_3, RVec<Double_t> eta_3, RVec<Double_t> phi_3, Double_t newM_3)
+{
+  size_t n_cand = pt_1.size();
+  RVec<Double_t> cand_inv_mass(n_cand,0.);
+
+  for(unsigned i=0; i<n_cand; ++i)
+  {
+    TLorentzVector p4_1;
+    TLorentzVector p4_2;
+    TLorentzVector p4_3;
+    p4_1.SetPtEtaPhiM(pt_1[i],eta_1[i],phi_1[i],newM_1);
+    p4_2.SetPtEtaPhiM(pt_2[i],eta_2[i],phi_2[i],newM_2);
+    p4_3.SetPtEtaPhiM(pt_3[i],eta_3[i],phi_3[i],newM_3);
+    cand_inv_mass[i]=(p4_1+p4_2+p4_3).M();
+  }
+
+  return cand_inv_mass;
+
+}
+
+RVec<Double_t> get_PtEtaPhi_newM_2PartInvMass(RVec<Double_t> pt_1, RVec<Double_t> eta_1, RVec<Double_t> phi_1, Double_t newM_1,
+		                              RVec<Double_t> pt_2, RVec<Double_t> eta_2, RVec<Double_t> phi_2, Double_t newM_2)
+{
+  size_t n_cand = pt_1.size();
+  RVec<Double_t> cand_inv_mass(n_cand,0.);
+
+  for(unsigned i=0; i<n_cand; ++i)
+  {
+    TLorentzVector p4_1;
+    TLorentzVector p4_2;
+    p4_1.SetPtEtaPhiM(pt_1[i],eta_1[i],phi_1[i],newM_1);
+    p4_2.SetPtEtaPhiM(pt_2[i],eta_2[i],phi_2[i],newM_2);
+    cand_inv_mass[i]=(p4_1+p4_2).M();
+  }
+
+  return cand_inv_mass;
+
+}
+
+RVec<Double_t> get_PxPyPzE_newM_3PartInvMass(RVec<Double_t> px_1, RVec<Double_t> py_1, RVec<Double_t> pz_1, RVec<Double_t> E_1, Double_t m_1,
+		                                 RVec<Double_t> px_2, RVec<Double_t> py_2, RVec<Double_t> pz_2, RVec<Double_t> E_2, Double_t m_2,
+		                                 RVec<Double_t> px_3, RVec<Double_t> py_3, RVec<Double_t> pz_3, RVec<Double_t> E_3, Double_t m_3)
+{
+  size_t n_cand = px_1.size();
+  RVec<Double_t> cand_inv_mass(n_cand,0.);
+
+  for(unsigned i=0; i<n_cand; ++i)
+  {
+    TLorentzVector p4_1;
+    TLorentzVector p4_2;
+    TLorentzVector p4_3;
+    p4_1.SetPxPyPzE(px_1[i],py_1[i],pz_1[i],E_1[i]);
+    p4_2.SetPxPyPzE(px_2[i],py_2[i],pz_2[i],E_2[i]);
+    p4_3.SetPxPyPzE(px_3[i],py_3[i],pz_3[i],E_3[i]);
+    TLorentzVector p4_newM_1;
+    TLorentzVector p4_newM_2;
+    TLorentzVector p4_newM_3;
+    p4_newM_1.SetPtEtaPhiM(p4_1.Pt(),p4_1.Eta(),p4_1.Phi(),m_1);
+    p4_newM_2.SetPtEtaPhiM(p4_2.Pt(),p4_2.Eta(),p4_2.Phi(),m_2);
+    p4_newM_3.SetPtEtaPhiM(p4_3.Pt(),p4_3.Eta(),p4_3.Phi(),m_3);
+    cand_inv_mass[i]=(p4_newM_1+p4_newM_2+p4_newM_3).M();
   }
 
   return cand_inv_mass;
