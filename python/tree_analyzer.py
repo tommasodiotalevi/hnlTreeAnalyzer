7d1253be48246700e72cc9a12f4c569e976d0364
llunerti <leonardo.lunerti@cern.ch>
2023-02-07 12:01:24 +0100

    sf application + other

--- python/tree_analyzer.py
+++ python/tree_analyzer.py
@@ -1,3 +1,5 @@
+#!/cvmfs/sft-nightlies.cern.ch/lcg/views/dev4/Tue/x86_64-centos7-gcc12-opt/bin/python
+
 import ROOT
 import sys
 import math
@@ -11,18 +13,24 @@ start_time = time.time()
 
 #script input arguments
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("cfg_filename"      ,                                     help="Path to the input configuration file")
-parser.add_argument("dataset_short_name",                                     help="Short name of the input sample to process")
-parser.add_argument("--saveOutputTree"  , action='store_true', default=False, help="Save an output tree (after all the cuts)")
-parser.add_argument("--saveSlimmedTree" , action='store_true', default=False, help="Save an output tree (before the cuts and after best candidate selection)")
-parser.add_argument("--noHistograms"    , action='store_true', default=False, help="Do not save histogram as output")
-parser.add_argument("--addSPlotWeight"  , action='store_true', default=False, help="Add splot weight for data")
-parser.add_argument("--skipSlimCuts"    , action='store_true', default=False, help="Skip best candidate selection (N.B. a tree where the best candidate has been already been selected has to be provided in the input cfg file)")
-parser.add_argument("--skipSelCuts"     , action='store_true', default=False, help="Do not apply selection cuts")
-parser.add_argument("--skipPUrw"        , action='store_true', default=False, help="Do not apply PU reweighting")
-parser.add_argument("--nThreads"        , type=int           , default=1    , help="Number of threads")
-parser.add_argument("--addTag"          , type=str           , default=""   , help="Tag output files")
-parser.add_argument("--ctauReweighting" , action='store_true', default=False, help="Include ctau reweighting")
+parser.add_argument("cfg_filename"        ,                                     help="Path to the input configuration file")
+parser.add_argument("dataset_short_name"  ,                                     help="Short name of the input sample to process")
+parser.add_argument("--saveOutputTree"    , action='store_true', default=False, help="Save an output tree (after all the cuts)")
+parser.add_argument("--saveSlimmedTree"   , action='store_true', default=False, help="Save an output tree (before the cuts and after best candidate selection)")
+parser.add_argument("--noHistograms"      , action='store_true', default=False, help="Do not save histogram as output")
+parser.add_argument("--addSPlotWeight"    , action='store_true', default=False, help="Add splot weight for data")
+parser.add_argument("--skipSlimCuts"      , action='store_true', default=False, help="Skip best candidate selection (N.B. a tree where the best candidate has been already been selected has to be provided in the input cfg file)")
+parser.add_argument("--skipSelCuts"       , action='store_true', default=False, help="Do not apply selection cuts")
+parser.add_argument("--skipPUrw"          , action='store_true', default=False, help="Do not apply PU reweighting")
+parser.add_argument("--skipTrigSF"        , action='store_true', default=False, help="Do not apply trigger scale factors")
+parser.add_argument("--nThreads"          , type=int           , default=1    , help="Number of threads")
+parser.add_argument("--addTag"            , type=str           , default=""   , help="Tag output files")
+parser.add_argument("--ctauReweighting"   , action='store_true', default=False, help="Include ctau reweighting")
+parser.add_argument("--applyMuDsPtCorr"   , action='store_true', default=False, help="Apply reweighting to mu from Ds to correct data/MC pt discrepancies")
+parser.add_argument("--applyMuHnlPtCorr"  , action='store_true', default=False, help="Apply reweighting to mu from Hnl to correct data/MC pt discrepancies")
+parser.add_argument("--applyMuDsIPSCorr"  , action='store_true', default=False, help="Apply reweighting to mu from Ds to correct data/MC IPS discrepancies")
+parser.add_argument("--applyMuHnlIPSCorr" , action='store_true', default=False, help="Apply reweighting to mu from Hnl to correct data/MC IPS discrepancies")
+parser.add_argument("--keep"              , nargs="*"          , default=[]   , help="Select which branches to keep in the final output tree")
 args = parser.parse_args()
 
 ROOT.EnableThreadSafety()
@@ -62,6 +70,10 @@ if args.skipSlimCuts:
     input_file_list = "slimmed_file_name_list"
     tree_name = "slimmed_tree"
 
+if args.addSPlotWeight and args.skipSelCuts and args.skipSlimCuts:
+    input_file_list = "final_file_name_list"
+    tree_name = "final_tree"
+
 #get input files
 inputFileName_list = ntuples[dataset_to_process][input_file_list]
 dataset_category = ntuples[dataset_to_process]["dataset_category"]
@@ -74,9 +86,67 @@ if dataset_category != "data" and not args.skipPUrw:
     """.format(config["pu_weight_input_file"],config["pu_weight_histo_name"])
     )
 
+#get trigger scale factors histogram
+if dataset_category != "data" and not args.skipTrigSF:
+    print("sf histogram from {}, {}".format(config["trigger_sf_input_file"],config["trigger_sf_histo_name"]))
+    print("eff data histogram from {}, {}".format(config["trigger_eff_data_input_file"],config["trigger_eff_data_histo_name"]))
+    print("eff mc histogram from {}, {}".format(config["trigger_eff_mc_input_file"],config["trigger_eff_mc_histo_name"]))
+    ROOT.gInterpreter.Declare("""
+    auto trigger_sf_file       = TFile::Open("{sf_file}");
+    auto trigger_eff_data_file = TFile::Open("{edata_file}");
+    auto trigger_eff_mc_file   = TFile::Open("{emc_file}");
+    auto h_trigger_sf       = trigger_sf_file->Get<TH2D>("{sfhisto_name}");
+    auto h_trigger_eff_data = trigger_eff_data_file->Get<TH2D>("{edatahisto_name}");
+    auto h_trigger_eff_mc   = trigger_eff_mc_file->Get<TH2D>("{emchisto_name}");
+    """.format(sf_file=config["trigger_sf_input_file"],
+               edata_file=config["trigger_eff_data_input_file"],
+               emc_file=config["trigger_eff_mc_input_file"],
+               edatahisto_name=config["trigger_eff_data_histo_name"],
+               emchisto_name=config["trigger_eff_mc_histo_name"],
+               sfhisto_name=config["trigger_sf_histo_name"])
+    )
+
+#get mu_Ds pt shape scale factors histogram
+if dataset_category != "data" and args.applyMuDsPtCorr:
+    print("pt shape correction sf from {}, {}".format(config["ds_pt_shape_sf_input_file"],config["ds_pt_shape_sf_histo_name"]))
+    ROOT.gInterpreter.Declare("""
+    auto ds_pt_shape_sf_file = TFile::Open("{}");
+    auto h_ds_pt_shape_sf = ds_pt_shape_sf_file->Get<TH1D>("{}");
+    """.format(config["ds_pt_shape_sf_input_file"],config["ds_pt_shape_sf_histo_name"])
+    )
+
+#get mu_Hnl pt shape scale factors histogram
+if dataset_category != "data" and args.applyMuHnlPtCorr:
+    print("pt shape correction sf from {}, {}".format(config["hnl_pt_shape_sf_input_file"],config["hnl_pt_shape_sf_histo_name"]))
+    ROOT.gInterpreter.Declare("""
+    auto hnl_pt_shape_sf_file = TFile::Open("{}");
+    auto h_hnl_pt_shape_sf = hnl_pt_shape_sf_file->Get<TH1D>("{}");
+    """.format(config["hnl_pt_shape_sf_input_file"],config["hnl_pt_shape_sf_histo_name"])
+    )
+
+#get mu_Ds IPS shape scale factors histogram
+if dataset_category != "data" and args.applyMuDsIPSCorr:
+    print("ips shape correction sf from {}, {}".format(config["ds_ips_shape_sf_input_file"],config["ds_ips_shape_sf_histo_name"]))
+    ROOT.gInterpreter.Declare("""
+    auto ds_ips_shape_sf_file = TFile::Open("{}");
+    auto h_ds_ips_shape_sf = ds_ips_shape_sf_file->Get<TH1D>("{}");
+    """.format(config["ds_ips_shape_sf_input_file"],config["ds_ips_shape_sf_histo_name"])
+    )
+
+#get mu_Hnl IPS shape scale factors histogram
+if dataset_category != "data" and args.applyMuHnlIPSCorr:
+    print("ips shape correction sf from {}, {}".format(config["hnl_ips_shape_sf_input_file"],config["hnl_ips_shape_sf_histo_name"]))
+    ROOT.gInterpreter.Declare("""
+    auto hnl_ips_shape_sf_file = TFile::Open("{}");
+    auto h_hnl_ips_shape_sf = hnl_ips_shape_sf_file->Get<TH1D>("{}");
+    """.format(config["hnl_ips_shape_sf_input_file"],config["hnl_ips_shape_sf_histo_name"])
+    )
+
 #define unit weights
-mc_weight = 1.
-pu_weight = 1.
+mc_weight  = 1.
+pu_weight  = 1.
+trigger_sf_ds = 1.
+trigger_sf_hnl = 1.
 
 # get generator weight for MC
 if dataset_category != "data":
@@ -109,7 +179,10 @@ dataset_name_label = input_file_name[input_file_name.find("_")+1:input_file_name
 # save slimmed tree only once without applying categorization
 slimmed_tree_has_been_saved = False
 if args.saveOutputTree:
-    ntuples[dataset_to_process]["final_file_name_list"] = []
+    key = "final_file_name_list"
+    if args.addTag != "":
+        key = key+"_"+str(args.addTag)
+    ntuples[dataset_to_process][key] = []
 
 reports = {}
 weighted_events_reports = {}
@@ -149,6 +222,7 @@ for cat in selection["categories"]:
         if dataset_category != "data" and not args.skipPUrw:
             pu_weight = "h_pu_weights->GetBinContent(h_pu_weights->FindBin(nPU_trueInt))"
         df = df.Define("pu_weight",str(pu_weight)) 
+
     
         df = df.Define("tot_weight","mc_weight*pu_weight")
 
@@ -163,7 +237,10 @@ for cat in selection["categories"]:
             df.Snapshot(config["slimmed_tree_output_name"],slim_outFullPath,df.GetColumnNames())
             slimmed_tree_has_been_saved = True
             print("Slimmed tree saved in {}".format(slim_outFullPath))
-            ntuples[dataset_to_process]["slimmed_file_name_list"] = [str(slim_outFullPath)]
+            key = "slimmed_file_name_list"
+            if args.addTag != "":
+                key = key+"_"+str(args.addTag)
+            ntuples[dataset_to_process][key] = [str(slim_outFullPath)]
             with open(config["ntuples_cfg_file_full_path"], "w") as f:
                 json.dump(ntuples,f, indent=4, sort_keys=True)
             print("{} updated".format(config["ntuples_cfg_file_full_path"])) 
@@ -178,6 +255,53 @@ for cat in selection["categories"]:
         for sel in cat["selection_cuts"]:
             df = df.Filter(sel["cut"],sel["printout"])
 
+        # IMPLEMENT THIS OPTION IN A MORE CLEVER WAY!!
+        if args.skipPUrw:
+            df = df.Redefine("tot_weight","mc_weight")
+
+        # define trigger scale factors for MC only
+        if dataset_category != "data" and not args.skipPUrw:
+            trigger_sf_ds  = "h_trigger_sf->GetBinContent(h_trigger_sf->FindBin(C_{mu1l}_pt,C_{mu1l}_BS_ips_xy))".format(mu1l=config["mu1_label"])
+            trigger_sf_hnl = "h_trigger_sf->GetBinContent(h_trigger_sf->FindBin(C_{mu2l}_pt,C_{mu2l}_BS_ips_xy))".format(mu2l=config["mu2_label"])
+            trigger_eff_data_ds = "h_trigger_eff_data->GetBinContent(h_trigger_eff_data->FindBin(C_{mu1l}_pt>100.0?99.9:C_{mu1l}_pt,C_{mu1l}_BS_ips_xy>500.0?499.9:C_{mu1l}_BS_ips_xy))".format(mu1l=config["mu1_label"])
+            trigger_eff_mc_ds   = "h_trigger_eff_mc->GetBinContent(h_trigger_eff_mc->FindBin(C_{mu1l}_pt>100.0?99.9:C_{mu1l}_pt,C_{mu1l}_BS_ips_xy>500.0?499.9:C_{mu1l}_BS_ips_xy))".format(mu1l=config["mu1_label"])
+            trigger_eff_data_hnl = "h_trigger_eff_data->GetBinContent(h_trigger_eff_data->FindBin(C_{mu2l}_pt>100.0?99.9:C_{mu2l}_pt,C_{mu2l}_BS_ips_xy>500.0?499.9:C_{mu2l}_BS_ips_xy))".format(mu2l=config["mu2_label"])
+            trigger_eff_mc_hnl   = "h_trigger_eff_mc->GetBinContent(h_trigger_eff_mc->FindBin(C_{mu2l}_pt>100.0?99.9:C_{mu2l}_pt,C_{mu2l}_BS_ips_xy>500.0?499.9:C_{mu2l}_BS_ips_xy))".format(mu2l=config["mu2_label"])
+            df = df.Define("trigger_sf_ds",str(trigger_sf_ds)) 
+            df = df.Define("trigger_sf_hnl",str(trigger_sf_hnl)) 
+            df = df.Define("trigger_eff_data_ds",str(trigger_eff_data_ds)) 
+            df = df.Define("trigger_eff_data_hnl",str(trigger_eff_data_hnl)) 
+            df = df.Define("trigger_eff_mc_ds",str(trigger_eff_mc_ds)) 
+            df = df.Define("trigger_eff_mc_hnl",str(trigger_eff_mc_hnl)) 
+            df = df.Define("C_{mu2l}_matched_HLT".format(mu2l=config["mu2_label"]),"(C_{mu2l}_matched_MU7_IP4>0 && C_{mu2l}_dr_MU7_IP4<0.005) || (C_{mu2l}_matched_MU8_IP3>0 && C_{mu2l}_dr_MU8_IP3<0.005) || (C_{mu2l}_matched_MU8_IP5>0 && C_{mu2l}_dr_MU8_IP5<0.005) || (C_{mu2l}_matched_MU8_IP6>0 && C_{mu2l}_dr_MU8_IP6<0.005) || (C_{mu2l}_matched_MU9_IP4>0 && C_{mu2l}_dr_MU9_IP4<0.005) || (C_{mu2l}_matched_MU9_IP5>0 && C_{mu2l}_dr_MU9_IP5<0.005) || (C_{mu2l}_matched_MU9_IP6>0 && C_{mu2l}_dr_MU9_IP6<0.005) || (C_{mu2l}_matched_MU10p5_IP3p5>0 && C_{mu2l}_dr_MU10p5_IP3p5<0.005) || (C_{mu2l}_matched_MU12_IP6>0 && C_{mu2l}_dr_MU12_IP6<0.005)".format(mu2l=config["mu2_label"])) 
+            df = df.Define("C_{mu1l}_matched_HLT".format(mu1l=config["mu1_label"]),"(C_{mu1l}_matched_MU7_IP4>0 && C_{mu1l}_dr_MU7_IP4<0.005) || (C_{mu1l}_matched_MU8_IP3>0 && C_{mu1l}_dr_MU8_IP3<0.005) || (C_{mu1l}_matched_MU8_IP5>0 && C_{mu1l}_dr_MU8_IP5<0.005) || (C_{mu1l}_matched_MU8_IP6>0 && C_{mu1l}_dr_MU8_IP6<0.005) || (C_{mu1l}_matched_MU9_IP4>0 && C_{mu1l}_dr_MU9_IP4<0.005) || (C_{mu1l}_matched_MU9_IP5>0 && C_{mu1l}_dr_MU9_IP5<0.005) || (C_{mu1l}_matched_MU9_IP6>0 && C_{mu1l}_dr_MU9_IP6<0.005) || (C_{mu1l}_matched_MU10p5_IP3p5>0 && C_{mu1l}_dr_MU10p5_IP3p5<0.005) || (C_{mu1l}_matched_MU12_IP6>0 && C_{mu1l}_dr_MU12_IP6<0.005)".format(mu1l=config["mu1_label"])) 
+            df = df.Define("trigger_sf","compute_total_sf(trigger_sf_ds,C_{mu1l}_matched_HLT,trigger_sf_hnl,C_{mu2l}_matched_HLT)".format(mu1l=config["mu1_label"],mu2l=config["mu2_label"]))
+            #df = df.Define("trigger_sf","compute_total_sf(trigger_eff_data_ds,trigger_eff_mc_ds,C_{mu1l}_matched_HLT,trigger_eff_data_hnl,trigger_eff_mc_hnl,C_{mu2l}_matched_HLT)".format(mu1l=config["mu1_label"],mu2l=config["mu2_label"]))
+            df = df.Redefine("tot_weight","tot_weight*trigger_sf")
+
+        # define mu_Ds pt shape scale factors for MC only
+        if dataset_category != "data" and args.applyMuDsPtCorr:
+            ds_pt_shape_sf  = "h_ds_pt_shape_sf->GetBinContent(h_ds_pt_shape_sf->FindBin(C_{}_pt))".format(config["mu1_label"])
+            df = df.Define("ds_pt_shape_sf",str(ds_pt_shape_sf)) 
+            df = df.Redefine("tot_weight","tot_weight*ds_pt_shape_sf")
+
+        # define mu_Ds pt shape scale factors for MC only
+        if dataset_category != "data" and args.applyMuHnlPtCorr:
+            hnl_pt_shape_sf  = "h_hnl_pt_shape_sf->GetBinContent(h_hnl_pt_shape_sf->FindBin(C_{}_pt))".format(config["mu2_label"])
+            df = df.Define("hnl_pt_shape_sf",str(hnl_pt_shape_sf)) 
+            df = df.Redefine("tot_weight","tot_weight*hnl_pt_shape_sf")
+
+        # define mu_Ds IPS shape scale factors for MC only
+        if dataset_category != "data" and args.applyMuDsIPSCorr:
+            ds_ips_shape_sf  = "h_ds_ips_shape_sf->GetBinContent(h_ds_ips_shape_sf->FindBin(C_{}_BS_ips_xy))".format(config["mu1_label"])
+            df = df.Define("ds_ips_shape_sf",str(ds_ips_shape_sf)) 
+            df = df.Redefine("tot_weight","tot_weight*ds_ips_shape_sf")
+
+        # define mu_Hnl IPS shape scale factors for MC only
+        if dataset_category != "data" and args.applyMuHnlIPSCorr:
+            hnl_ips_shape_sf  = "h_hnl_ips_shape_sf->GetBinContent(h_hnl_ips_shape_sf->FindBin(C_{}_BS_ips_xy))".format(config["mu2_label"])
+            df = df.Define("hnl_ips_shape_sf",str(hnl_ips_shape_sf)) 
+            df = df.Redefine("tot_weight","tot_weight*hnl_ips_shape_sf")
         
         #get mc truth in case of signal sample
         if dataset_category == "signal":
@@ -203,21 +327,33 @@ for cat in selection["categories"]:
             finalTree_outFullPath = os.path.join(finalTree_outputDirName,finalTree_outputFileName)
             finalCSV_outFullPath = os.path.join(finalTree_outputDirName,finalCSV_outputFileName)
             #save output tree
-            df.Snapshot(config["tree_output_name"],finalTree_outFullPath,list(df.GetColumnNames()))
+            var_list = list(df.GetColumnNames())
+            csv_var_list = [x for x in df.GetColumnNames() if x.find("C_")==0 or x.find("ctau_weight")==0 or x.find("tot_weight")==0]
+            var_keep_list = args.keep
+            if len(var_keep_list)>0:
+                var_list = var_keep_list
+                csv_var_list = var_keep_list
+            df.Snapshot(config["tree_output_name"],finalTree_outFullPath,var_list)
             #save output csv
-            a = df.AsNumpy([x for x in df.GetColumnNames() if x.find("C_")==0 or x.find("ctau_weight")==0])
+            a = df.AsNumpy(csv_var_list)
             arr = numpy.array([x for x in a.values()]).transpose()
             numpy.savetxt(finalCSV_outFullPath, arr, delimiter=',', header=",".join([str(x) for x in a.keys()]), comments='')
             print("Output tree saved in {}".format(finalTree_outFullPath))
             print("Output csv saved in {}".format(finalCSV_outFullPath))
-            ntuples[dataset_to_process]["final_file_name_list"] += [str(finalTree_outFullPath)]
+            key = "final_file_name_list"
+            if args.addTag != "":
+                key = key+"_"+str(args.addTag)
+            ntuples[dataset_to_process][key] += [str(finalTree_outFullPath)]
             with open(config["ntuples_cfg_file_full_path"], "w") as f:
                 json.dump(ntuples,f, indent=4, sort_keys=True)
             print("{} updated".format(config["ntuples_cfg_file_full_path"]))
 
     if dataset_category=="data" and args.addSPlotWeight:
-        sdf = ROOT.RDataFrame(config["splot_weight_tree_name"],config["splot_weight_input_file"]) # get tree containing splot weights
-        asw = sdf.AsNumpy([config["splot_weight_variable"]])[config["splot_weight_variable"]] # get column of splot weights
+        #sdf = ROOT.RDataFrame(config["splot_weight_tree_name"],config["splot_weight_input_file"]) # get tree containing splot weights
+        #asw = sdf.AsNumpy([config["splot_weight_variable"]])[config["splot_weight_variable"]] # get column of splot weights
+        print("Input splot weight file: {}".format(ntuples[dataset_to_process]["splot_weight_input_file"]))
+        sdf = ROOT.RDataFrame(ntuples[dataset_to_process]["splot_weight_tree_name"],ntuples[dataset_to_process]["splot_weight_input_file"]) # get tree containing splot weights
+        asw = sdf.AsNumpy([ntuples[dataset_to_process]["splot_weight_variable"]])[ntuples[dataset_to_process]["splot_weight_variable"]] # get column of splot weights
         print("entries in splot tree: {}".format(len(asw)))
         print("entries in analyzed tree: {}".format(df.Count().GetValue()))
         if len(asw) != df.Count().GetValue():
@@ -299,3 +435,5 @@ for c in reports:
             print("{} weighted events: {}".format(w,weighted_events_reports[c][w]))
 
 print("--- Analysis completed in {} seconds ---".format(time.time() - start_time))
+
+
