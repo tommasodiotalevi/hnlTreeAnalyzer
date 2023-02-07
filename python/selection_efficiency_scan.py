5247dfa2f928865a4a51b0c0a5006c5565853dd5
llunerti <leonardo.lunerti@cern.ch>
2023-02-07 12:00:05 +0100

    add weighted count

--- python/selection_efficiency_scan.py
+++ python/selection_efficiency_scan.py
@@ -12,7 +12,7 @@ start_time = time.time()
 #script input arguments
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("cfg_filename"         ,                                     help="Path to the input configuration file")
-parser.add_argument("--datasetsToProcess"  , nargs='*'          , default=[]   , help="List of short names of the input sample to process")
+parser.add_argument("--datasetsToProcess"  , nargs='*'          , default=[]   , help="Short name of the input sample to process")
 parser.add_argument("--nThreads"           , type=int           , default=1    , help="Number of threads")
 parser.add_argument("--nCuts"              , type=int           , default=20   , help="Number of cuts for the efficiency scan")
 args = parser.parse_args()
@@ -22,10 +22,6 @@ ROOT.EnableImplicitMT(args.nThreads)
 
 configFileName      = args.cfg_filename
 datasets_to_process = args.datasetsToProcess
-
-# let private function to be avaliable in RDataFrame evn
-header_path = "/home/CMS-T3/lunerti/hnlTreeAnalyzer/interface/df_tools.h"
-ROOT.gInterpreter.Declare('#include "{}"'.format(header_path))
 
 #open analyzer configuration file
 with open(configFileName, "r") as f:
@@ -39,7 +35,17 @@ with open(config["ntuples_cfg_file_full_path"], "r") as f:
 with open(config["selection_cfg_file_full_path"], "r") as f:
     selection = json.loads(f.read())
 
-input_file_list = "final_file_name_list"
+# if it's true assign a label to the output file
+isasignalsample = False
+mass_label = str()
+ctau_label = str()
+if len(datasets_to_process)==1 and ntuples[datasets_to_process[0]]["dataset_category"]=="signal":
+    isasignalsample = True
+    ss = datasets_to_process[0]
+    mass_label = ss[ss.find("mN")+2:ss.find("mN")+5]
+    ctau_label = ss[ss.find("ctau")+4:ss.rfind("p0")+2]
+
+input_file_list = "final_file_name_list_noOpt"
 tree_name = "final_tree"
 
 #get input files
@@ -50,8 +56,6 @@ for dataset_to_process in datasets_to_process:
     if ntuples[dataset_to_process]["dataset_category"] != dataset_category:
         print("Please provide input samples of the same category (e.g. all background samples)")
         exit(0)
-
-print("----> {}".format(inputFileName_list))
  
 n_cuts = args.nCuts
 reports = {}
@@ -59,11 +63,11 @@ reports = {}
 
 #build a dictionary that groups all the files
 #of the same catogory togheter
-
 input_file_catOrdered = {}
 for cat in selection["categories"]:
     file_list = []
     for inputFileName in inputFileName_list:
+        print("-->{}".format(inputFileName))
         file_cat_label = inputFileName.split("/")[-1].split(".")[0].split("_")[-1]
         if file_cat_label == cat["label"]:
             file_list.append(inputFileName)
@@ -73,6 +77,12 @@ print(input_file_catOrdered)
 
 
 for cat in selection["categories"]:
+
+
+    print("*** {} category".format(cat["label"]))
+
+    if cat["label"] == "inclusive":
+        continue
 
     chain = ROOT.TChain(tree_name)
     
@@ -93,7 +103,8 @@ for cat in selection["categories"]:
         for sel in selection["gen_matching_cuts"]:
             df = df.Filter(sel["cut"],sel["printout"])
 
-    tot_events = df.Count().GetValue()
+    #tot_events = df.Count().GetValue()
+    tot_events = df.Sum("tot_weight").GetValue()
 
     #build signal selection efficiency csv file 
     var_header = "entry"
@@ -101,23 +112,18 @@ for cat in selection["categories"]:
     df_array = numpy.arange(n_cuts)
     for var in selection["selection_eff_scan"]:
         a_cuts   = numpy.array(numpy.linspace(var["low_edge"],var["up_edge"],n_cuts))
-        pass_events = [df.Filter(str(var["name"])+var["logic"]+str(cut)).Count().GetValue() for cut in a_cuts]
-        fail_events = [(tot_events-df.Filter(str(var["name"])+var["logic"]+str(cut)).Count().GetValue()) for cut in a_cuts]
+        pass_events = [df.Filter(str(var["name"])+var["logic"]+str(cut)).Sum("tot_weight").GetValue() for cut in a_cuts]
+        fail_events = [tot_events-float(df.Filter(str(var["name"])+var["logic"]+str(cut)).Sum("tot_weight").GetValue()) for cut in a_cuts]
         a_pass = numpy.array(pass_events)
         a_fail = numpy.array(fail_events)
         var_header += ","+var["name"]+"_cut,"+var["name"]+"_pass,"+var["name"]+"_fail"
         fmt += ", %.2f, %.1f, %.1f"
         df_array = numpy.column_stack((df_array,a_cuts,a_pass,a_fail))
     output_path = os.path.join(outputDirName,"sel_eff_tree_"+dataset_category+"_"+cat["label"]+".csv")
+    if isasignalsample:
+        output_path = os.path.join(outputDirName,"sel_eff_tree_"+dataset_category+"_mN{}_ctau{}_".format(mass_label,ctau_label)+cat["label"]+".csv")
     numpy.savetxt(output_path, df_array, delimiter=',', header=var_header, comments='',fmt=fmt)
     print("Output saved in {}".format(output_path))
-
-    #reports[cat["label"]] = df.Report()
-
-#print("+++ FINAL REPORT +++")
-#for c in reports:
-#    print("--> {} category".format(c))
-#    reports[c].Print()
 
 print("--- Analysis completed in {} seconds ---".format(time.time() - start_time))
 
